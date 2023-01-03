fd2c85a78adcfcb9df9ad41c09a0becf3feda1eb
Thomas Guillod <guillod@otvam.ch>
2023-01-03 16:28:50 -0500

    add exception for invalid data

--- solver/check_data.py
+++ solver/check_data.py
@@ -1,14 +1,21 @@
 import numpy as np
 
 
+class CheckError(Exception):
+    pass
+
+
 def __check_conductor(idx_v, conductor):
     for dat_tmp in conductor:
         idx = dat_tmp["idx"]
         rho = dat_tmp["rho"]
 
-        assert np.isscalar(rho), "conductor resistivity should be a scalar"
-        assert np.isreal(rho), "conductor resistivity should be real"
-        assert rho > 0, "conductor resistivity should be greater than zero"
+        if not np.isscalar(rho):
+            raise CheckError("conductor resistivity should be a scalar")
+        if not np.isreal(rho):
+            raise CheckError("conductor resistivity should be real")
+        if not (rho > 0):
+            raise CheckError("conductor resistivity should be greater than zero")
 
         idx_v = np.append(idx_v, np.array(idx))
 
@@ -21,8 +28,10 @@ def __check_src(idx_src, tag_src, src):
         idx = dat_tmp["idx"]
         value = dat_tmp["value"]
 
-        assert np.isscalar(value), "source value should be a scalar"
-        assert isinstance(tag, str), "source name should be a string"
+        if not np.isscalar(value):
+            raise CheckError("source value should be a scalar")
+        if not isinstance(tag, str):
+            raise CheckError("source name should be a string")
 
         tag_src.append(tag)
         idx_src = np.append(idx_src, np.array(idx))
@@ -37,41 +46,24 @@ def check_voxel(data_solver):
     n_green_simplify = data_solver["n_green_simplify"]
 
     # check size
-    assert len(n) == 3, "invalid voxel size"
-    assert len(d) == 3, "invalid voxel size"
+    if not (len(n) == 3):
+        raise CheckError("invalid voxel size")
+    if not (len(d) == 3):
+        raise CheckError("invalid voxel size")
 
     # extract the voxel data
     (nx, ny, nz) = n
     (dx, dy, dz) = d
 
     # check value
-    assert nx >= 1, "number of voxels cannot be smaller than one"
-    assert ny >= 1, "number of voxels cannot be smaller than one"
-    assert nz >= 1, "number of voxels cannot be smaller than one"
-    assert dx > 0, "voxel dimension cannot be zero or smaller"
-    assert dy > 0, "voxel dimension cannot be zero or smaller"
-    assert dz > 0, "voxel dimension cannot be zero or smaller"
-    assert n_green_simplify > 0, "voxel distance to simplify the green function cannot be zero of smaller"
+    if not ((nx >= 1) and (ny >= 1) and (nz >= 1)):
+        raise CheckError("number of voxels cannot be smaller than one")
+    if not ((dx > 0) and (dy > 0) and (dz > 0)):
+        raise CheckError("voxel dimension cannot be zero or smaller")
+    if not (n_green_simplify > 0):
+        raise CheckError("voxel distance to simplify the green function cannot be zero of smaller")
 
     return n, d, n_green_simplify
-
-
-def check_solver(data_solver):
-    # extract field
-    freq = data_solver["freq"]
-    solver_options = data_solver["solver_options"]
-
-    # check frequency
-    assert freq >= 0, "frequency cannot be negative"
-
-    # check solver options
-    assert isinstance(solver_options, dict), "solver options should be a dict"
-    assert solver_options["tol"] > 0, "solver relative tolerance should be greater than zero"
-    assert solver_options["atol"] > 0, "solver absolute tolerance should be greater than zero"
-    assert solver_options["restart"] >= 1, "number of iterations between restarts should be greater than zero"
-    assert solver_options["restart"] >= 1, "number of restart cycles should be greater than zero"
-
-    return freq, solver_options
 
 
 def check_problem(data_solver):
@@ -86,9 +78,12 @@ def check_problem(data_solver):
     n = nx*ny*nz
 
     # check type
-    assert isinstance(conductor, list), "solver options should be a dict"
-    assert isinstance(src_current, list), "solver options should be a dict"
-    assert isinstance(src_voltage, list), "solver options should be a dict"
+    if not isinstance(conductor, list):
+        raise CheckError("solver options should be a dict")
+    if not isinstance(src_current, list):
+        raise CheckError("solver options should be a dict")
+    if not isinstance(src_voltage, list):
+        raise CheckError("solver options should be a dict")
 
     # check the conductor
     idx_v = np.array([], dtype=np.int64)
@@ -101,15 +96,47 @@ def check_problem(data_solver):
     (idx_src, tag_src) = __check_src(idx_src, tag_src, src_voltage)
 
     # check for unicity
-    assert len(np.unique(idx_v)) == len(idx_v), "conductor indices should be unique"
-    assert len(np.unique(idx_src)) == len(idx_src), "source indices should be unique"
-    assert len(np.unique(tag_src)) == len(tag_src), "source tag should be unique"
+    if not (len(np.unique(idx_v)) == len(idx_v)):
+        raise CheckError("conductor indices should be unique")
+    if not (len(np.unique(idx_src)) == len(idx_src)):
+        raise CheckError("source indices should be unique")
+    if not (len(np.unique(tag_src)) == len(tag_src)):
+        raise CheckError("source tag should be unique")
 
     # check for indices range
-    assert np.all(idx_v >= 0) and np.all(idx_v < n) , "conductor indices should belong to the voxel structure"
-    assert np.all(idx_src >= 0) and np.all(idx_src < n) , "source indices should belong to the voxel structure"
+    if not (np.all(idx_v >= 0) and np.all(idx_v < n)):
+        raise CheckError("conductor indices should belong to the voxel structure")
+    if not (np.all(idx_src >= 0) and np.all(idx_src < n)):
+        raise CheckError("source indices should belong to the voxel structure")
 
     # check that the terminal indices are conductor indices
-    assert np.all(np.isin(idx_src, idx_v)), "source indices are not included in conductor indices"
+    if not np.all(np.isin(idx_src, idx_v)):
+        "source indices are not included in conductor indices"
 
     return conductor, src_current, src_voltage
+
+
+def check_solver(data_solver):
+    # extract field
+    freq = data_solver["freq"]
+    solver_options = data_solver["solver_options"]
+
+    # check frequency
+    if not(freq >= 0):
+        raise CheckError("frequency cannot be negative")
+
+    # check solver options
+    if not isinstance(solver_options, dict):
+        raise CheckError("solver options should be a dict")
+    if not (solver_options["tol"] > 0):
+        raise CheckError("solver relative tolerance should be greater than zero")
+    if not (solver_options["atol"] > 0):
+        raise CheckError("solver absolute tolerance should be greater than zero")
+    if not (solver_options["restart"] >= 1):
+        raise CheckError("number of iterations between restarts should be greater than zero")
+    if not (solver_options["maxiter"] >= 1):
+        raise CheckError("number of restart cycles should be greater than zero")
+    if not (solver_options["condmax"] > 0):
+        raise CheckError("maximum condition number should be greater than zero")
+
+    return freq, solver_options
