737f81e67c78ccffc08ad0af6e40eac7833f57ba
Thomas Guillod <guillod@otvam.ch>
2023-01-04 21:14:08 -0500

    PyVista natice code (X11 bug), Qt might be better

--- main/plotter.py
+++ main/plotter.py
@@ -6,7 +6,12 @@ from main import logging_utils
 logger = logging_utils.get_logger("plotter", "INFO")
 
 
-def __extract_voxel():
+def __extract_voxel(data_res):
+    """
+    Convert the voxel geometry into an unstructured grid.
+    Add the solver results (field, material, resistivity) to the grid.
+    """
+
     n = data_res["n"]
     d = data_res["d"]
     ori = data_res["ori"]
@@ -16,25 +21,24 @@ def __extract_voxel():
     J_voxel = data_res["J_voxel"]
     src_terminal = data_res["src_terminal"]
 
-    plot_options = {
-        "grid_plot": True,
-        "grid_thickness": 1.0,
-        "grid_opacity": 0.1,
-        "geom_plot": True,
-        "geom_thickness": 1.0,
-        "geom_opacity": 0.5,
-    }
-
+    # convert the voxel geometry into a unstructured grid.
     (grid, geom) = manage_voxel.get_geom(n, d, ori, idx_voxel)
+
+    # add the solution to the grid
     geom = manage_voxel.get_material(idx_voxel, geom, src_terminal)
     geom = manage_voxel.get_resistivity(idx_voxel, geom, rho_voxel)
     geom = manage_voxel.get_potential(idx_voxel, geom, V_voxel)
     geom = manage_voxel.get_current_density(idx_voxel, geom, J_voxel)
 
+    return grid, geom
+
 
 def run(data_res, data_plotter):
     # init
     logger.info("INIT")
+
+    logger.info("parse the voxel geometry and the data")
+    (grid, geom) = __extract_voxel(data_res)
 
     # end
     logger.info("END")
