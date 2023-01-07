4e8f37f3a397c17fb39a7d0dc3f8b97b1a6fc1a3
Thomas Guillod <guillod@otvam.ch>
2023-01-07 18:01:43 -0500

    Code for transforming image into voxels (without working example).

--- data_input/data_solver_pcb_trf.py
+++ data_input/data_solver_pcb_trf.py
@@ -1,4 +1,95 @@
-def _add_image(conductor, source, n, data):
+import imageio.v3 as iio
+import numpy as np
+
+
+def _get_idx_image(nx, ny, img, color):
+    # check image
+    assert img.shape == (nx, ny, 4), "invalid image size"
+
+    # init idx
+    idx_img = np.array([], dtype=np.int64)
+
+    # get the indices
+    for color_tmp in color:
+        # get the color vector
+        color_tmp = np.array(color_tmp)
+        color_tmp = np.expand_dims(color_tmp, axis=(0, 1))
+        assert color_tmp.shape == (1, 1, 4), "invalid image size"
+
+        # find the color
+        idx_tmp = np.all(img == color_tmp, axis=2)
+        idx_tmp = idx_tmp.flatten(order="F")
+        idx_tmp = np.flatnonzero(idx_tmp)
+
+        # add the indices to the array
+        idx_img = np.append(idx_img, idx_tmp)
+
+    return idx_img
+
+
+def _get_idx_voxel(nx, ny, nz, idx_z, idx_img):
+    # init idx
+    idx = np.array([], dtype=np.int64)
+
+    # convert image indices into voxel indices
+    for idx_tmp in idx_z:
+        # check size
+        assert (idx_tmp >= 0) and (idx_tmp < nz), "invalid image size"
+
+        # convert indices
+        idx_tmp = idx_tmp*nx*ny+idx_img
+
+        # add the indices to the array
+        idx = np.append(idx, idx_tmp)
+
+    return idx
+
+
+def _get_image(filename):
+    img = iio.imread(filename)
+    img = np.swapaxes(img, 0, 1)
+    img = np.flip(img, axis=1)
+
+    return img
+
+def _add_layer(conductor, source, n, data):
+    # extract the data
+    idx_z = data["idx_z"]
+    filename = data["filename"]
+    conductor_color = data["conductor_color"]
+    source_color = data["source_color"]
+
+    # extract the voxel data_output
+    (nx, ny, nz) = n
+
+    # read the image
+    img = _get_image(filename)
+
+    # add conductor
+    for tag, dat_tmp in conductor_color.items():
+        # get the data
+        color = dat_tmp["color"]
+        rho = dat_tmp["rho"]
+
+        # get the indices
+        idx_img = _get_idx_image(nx, ny, img, color)
+        idx = _get_idx_voxel(nx, ny, nz, idx_z, idx_img)
+
+        # add the conductor
+        conductor[tag] = {"idx": idx, "rho": rho}
+
+    # add conductor
+    for tag, dat_tmp in source_color.items():
+        color = dat_tmp["color"]
+        source_type = dat_tmp["source_type"]
+        value = dat_tmp["value"]
+
+        # get the indices
+        idx = _get_idx_image(nx, ny, img, color)
+        idx = _get_idx_voxel(nx, ny, nz, idx_z, idx)
+
+        # add the source
+        conductor[tag] = {"idx": idx, "source_type": source_type, "value": value}
 
     return conductor, source
 
@@ -17,18 +108,23 @@ def get_data():
     conductor = {}
     source = {}
 
+    # color
+    color_conductor = [[0, 0, 0, 255], [0, 255, 0, 255], [255, 0, 0, 255]]
+    color_sink = [[0, 0, 0, 255]]
+    color_source = [255, 0, 0, 255]
+
     # stack
     data = {
         "idx_z": [0],
         "filename": "pcb_trf/return.png",
-        "conductor": {
-            "c_1_return": {"color": [[255, 255, 255]], "rho": 1.7544e-08},
+        "conductor_color": {
+            "c_1_return": {"color": color_conductor, "rho": 1.7544e-08},
         },
-        "source": {
-            "src_1": {"color": [[255, 255, 255]], "source_type": "current", "value": +1},
+        "source_color": {
+            "src_1": {"color": color_sink, "source_type": "voltage", "value": 0},
         },
     }
-    (conductor, source), _add_image(conductor, source, n, data)
+    (conductor, source), _add_layer(conductor, source, n, data)
 
     # assign results
     data_solver = {
@@ -47,3 +143,4 @@ def get_data():
     return data_solver
 
 
+get_data()
