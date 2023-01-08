575717f74cc4696da01a91ac6baee465e94241ec
Thomas Guillod <guillod@otvam.ch>
2023-01-08 12:48:28 -0500

    Better image to voxel conversion.

--- data_input/data_solver_pcb_trf.py
+++ data_input/data_solver_pcb_trf.py
@@ -27,17 +27,14 @@ def _get_idx_image(nx, ny, img, color):
     return idx_img
 
 
-def _get_idx_voxel(nx, ny, nz, idx_z, idx_img):
+def _get_idx_voxel(nx, ny, nz, n_add, idx_img):
     # init idx
     idx = np.array([], dtype=np.int64)
 
     # convert image indices into voxel indices
-    for idx_tmp in idx_z:
-        # check size
-        assert (idx_tmp >= 0) and (idx_tmp < nz), "invalid image size"
-
+    for n_tmp in range(n_add):
         # convert indices
-        idx_tmp = idx_tmp*nx*ny+idx_img
+        idx_tmp = (nz+n_tmp)*nx*ny+idx_img
 
         # add the indices to the array
         idx = np.append(idx, idx_tmp)
@@ -52,50 +49,45 @@ def _get_image(filename):
 
     return img
 
-def _add_layer(conductor, source, n, data):
+
+def _get_color_indices(data, nx, ny, nz, n_add, img, data_color):
+    for tag_tmp, color_tmp in data_color.items():
+        # get the indices
+        idx_img = _get_idx_image(nx, ny, img, color_tmp)
+        idx_add = _get_idx_voxel(nx, ny, nz, n_add, idx_img)
+
+        # add the indices
+        idx = data[tag_tmp]["idx"]
+        idx = np.array(idx, dtype=np.int64)
+        idx = np.append(idx, idx_add)
+        data[tag_tmp]["idx"] = idx
+
+    return data
+
+
+def _add_layer(conductor, source, nx, ny, nz, data):
     # extract the data
-    idx_z = data["idx_z"]
+    n_add = data["n_add"]
     filename = data["filename"]
     conductor_color = data["conductor_color"]
     source_color = data["source_color"]
 
-    # extract the voxel data_output
-    (nx, ny, nz) = n
-
     # read the image
     img = _get_image(filename)
 
-    # add conductor
-    for tag, dat_tmp in conductor_color.items():
-        # get the data
-        color = dat_tmp["color"]
-        rho = dat_tmp["rho"]
+    # add the indices
+    conductor = _get_color_indices(conductor, nx, ny, nz, n_add, img, conductor_color)
+    source = _get_color_indices(source, nx, ny, nz, n_add, img, source_color)
 
-        # get the indices
-        idx_img = _get_idx_image(nx, ny, img, color)
-        idx = _get_idx_voxel(nx, ny, nz, idx_z, idx_img)
+    # update the layer stack
+    nz += n_add
 
-        # add the conductor
-        conductor[tag] = {"idx": idx, "rho": rho}
-
-    # add conductor
-    for tag, dat_tmp in source_color.items():
-        color = dat_tmp["color"]
-        source_type = dat_tmp["source_type"]
-        value = dat_tmp["value"]
-
-        # get the indices
-        idx = _get_idx_image(nx, ny, img, color)
-        idx = _get_idx_voxel(nx, ny, nz, idx_z, idx)
-
-        # add the source
-        conductor[tag] = {"idx": idx, "source_type": source_type, "value": value}
-
-    return conductor, source
+    return nz, conductor, source
 
 
 def get_data():
-    n = (176, 160, 20)
+    nx = 176
+    ny = 160
     d = (50e-6, 50e-6, 50e-6)
     r = (1, 1, 1)
     ori = (0, 0, 0)
@@ -104,31 +96,39 @@ def get_data():
     solver_options = {"tol": 1e-6, "atol": 1e-12, "restart": 20, "maxiter": 100}
     condition_options = {"check": True, "tolerance": 1e9, "accuracy": 2}
 
-    # init geometry description
-    conductor = {}
-    source = {}
-
-    # color
+    # layer parameters
     color_conductor = [[0, 0, 0, 255], [0, 255, 0, 255], [255, 0, 0, 255]]
     color_sink = [[0, 0, 0, 255]]
     color_source = [255, 0, 0, 255]
 
+    # init stack
+    nz = 0
+
+    conductor = {
+        "pri": {"idx": [], "rho": 1.7544e-08},
+        "sec": {"idx": [], "rho": 8.7720e-08},
+    }
+    source = {
+        "pri_src": {"source_type": "current", "idx": [], "value": 1},
+        "pri_sink": {"source_type": "voltage", "idx": [], "value": 0},
+        "sec_src": {"source_type": "current", "idx": [], "value": 1},
+        "sec_sink": {"source_type": "voltage", "idx": [], "value": 0},
+    }
+
     # stack
     data = {
-        "idx_z": [0],
+        "n_add": 1,
         "filename": "pcb_trf/return.png",
-        "conductor_color": {
-            "c_1_return": {"color": color_conductor, "rho": 1.7544e-08},
-        },
-        "source_color": {
-            "src_1": {"color": color_sink, "source_type": "voltage", "value": 0},
-        },
+        "conductor_color": {"pri": color_conductor},
+        "source_color": {"pri_sink": color_sink},
     }
-    (conductor, source), _add_layer(conductor, source, n, data)
+    (nz, conductor, source), _add_layer(conductor, source, nx, ny, nz, data)
+
+    # get voxel size
+    n = (nx, ny, nz)
 
     # assign results
     data_solver = {
-        "n": n,
         "d": d,
         "r": r,
         "ori": ori,
