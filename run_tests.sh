ef6930de592ba7acf46d01b520bf5c2f338e3b95
Thomas Guillod <guillod@otvam.ch>
2023-01-17 09:49:14 -0500

    Bash scripts for running the tests.

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Script for copying the examples data to the tests.
+# Script for copying the examples data to the tests and running the tests.
 #
 # Thomas Guillod
 # (c) 2023 - Dartmouth College
@@ -22,12 +22,14 @@ function copy_folder {
   cp -R $SRC $DST
 }
 
-#######################################################################
+echo "================================================================"
+echo "COPY THE FILES"
+echo "================================================================"
+
 echo "copy the viewer and plotter configuration files"
 copy_file "data_visualization" "data_viewer.json"
 copy_file "data_visualization" "data_plotter.json"
 
-#######################################################################
 echo "copy the mesher files"
 copy_file "data_mesher" "png_inductor.json"
 copy_file "data_mesher" "stl_inductor.json"
@@ -36,17 +38,20 @@ copy_file "data_mesher" "voxel_transformer.json"
 copy_folder "data_mesher" "png_inductor"
 copy_folder "data_mesher" "stl_inductor"
 
-#######################################################################
 echo "copy the problem files"
 copy_file "data_problem" "voxel_slab.json"
 copy_file "data_problem" "voxel_transformer.json"
 
-#######################################################################
 echo "copy the voxel files"
 copy_file "data_voxel" "voxel_slab.pck"
 copy_file "data_voxel" "voxel_transformer.pck"
 
-#######################################################################
+echo "================================================================"
+echo "RUN THE TESTS"
+echo "================================================================"
+
+python -m unittest -v
+
 exit 0
 
 
