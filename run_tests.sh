90c7a23e897bed8ce12e052dcd6170080ae3db54
Thomas Guillod <guillod@otvam.ch>
2023-01-18 16:31:03 -0500

    New test structure (complete workflow)

--- run_tests.sh
+++ run_tests.sh
@@ -4,53 +4,23 @@
 # Thomas Guillod
 # (c) 2023 - Dartmouth College
 
-function copy_file {
-  FOLDER=$1
-  NAME=$2
-
-  SRC="examples/${FOLDER}/${NAME}"
-  DST="tests/${FOLDER}/${NAME}"
-  cp $SRC $DST
-}
-
 function copy_folder {
-  FOLDER=$1
-  NAME=$2
-
-  SRC="examples/${FOLDER}/${NAME}"
-  DST="tests/${FOLDER}"
-  cp -R $SRC $DST
+  echo "copy / $1"
+  rm -rf tests/$1
+  rsync -ma --include '*/' --include '*.json' --exclude '*' examples/$1 tests
+  rsync -ma --include '*/' --include '*.png' --exclude '*' examples/$1 tests
+  rsync -ma --include '*/' --include '*.stl' --exclude '*' examples/$1 tests
 }
 
 echo "================================================================"
 echo "COPY THE FILES"
 echo "================================================================"
 
-echo "copy the viewer and plotter configuration files"
-copy_file "data_visualization" "data_viewer.json"
-copy_file "data_visualization" "data_plotter.json"
-
-echo "copy the mesher files"
-copy_file "data_mesher" "png_inductor.json"
-copy_file "data_mesher" "stl_inductor.json"
-copy_file "data_mesher" "voxel_slab.json"
-copy_file "data_mesher" "voxel_transformer.json"
-copy_folder "data_mesher" "png_inductor"
-copy_folder "data_mesher" "stl_inductor"
-
-echo "copy the point files"
-copy_file "data_point" "png_inductor.json"
-copy_file "data_point" "stl_inductor.json"
-copy_file "data_point" "voxel_slab.json"
-copy_file "data_point" "voxel_transformer.json"
-
-echo "copy the problem files"
-copy_file "data_problem" "voxel_slab.json"
-copy_file "data_problem" "voxel_transformer.json"
-
-echo "copy the voxel files"
-copy_file "data_voxel" "voxel_slab.pck"
-copy_file "data_voxel" "voxel_transformer.pck"
+copy_folder data_visualization
+copy_folder data_voxel_slab
+copy_folder data_voxel_transformer
+copy_folder data_png_inductor
+copy_folder data_stl_inductor
 
 echo "================================================================"
 echo "RUN THE TESTS"
---
+++ tests/test_workflow.py
@@ -0,0 +1,99 @@
+"""
+Integration test for the complete workflow.
+True unit tests are not implemented.
+
+The testing is done with the unittest library.
+"""
+
+__author__ = "Thomas Guillod"
+__copyright__ = "(c) 2023 - Dartmouth College"
+
+import os
+import unittest
+import tempfile
+import logging
+from PyPEEC import script
+
+# disable logging to prevent clutter during test evaluation
+logging.disable(logging.CRITICAL)
+
+# get the path the folder
+path_root = os.path.dirname(__file__)
+
+
+class TestMesherViewer(unittest.TestCase):
+    """
+    Test the workflow different cases:
+        - run the mesher
+        - run the viewer
+        - run the solver
+        - run the plotter
+
+    The intermediate file are stored with temporary files.
+    """
+
+    def _test_workflow(self, name):
+        """
+        Test mesher/viewer for a specific test case.
+        """
+
+        # get input file name
+        file_mesher = os.path.join(path_root, name, "mesher.json")
+        file_point = os.path.join(path_root, name, "point.json")
+        file_problem = os.path.join(path_root, name, "problem.json")
+        file_plotter = os.path.join(path_root, "data_visualization", "data_plotter.json")
+        file_viewer = os.path.join(path_root, "data_visualization", "data_viewer.json")
+
+        # get the temped file name
+        fid_voxel = tempfile.NamedTemporaryFile(suffix='.pck')
+        fid_solution = tempfile.NamedTemporaryFile(suffix='.pck')
+        file_voxel = fid_voxel.name
+        file_solution = fid_solution.name
+
+        # run the mesher
+        status = script.run_mesher(file_mesher, file_voxel)
+        self.assertTrue(status, msg="mesher failure")
+
+        # run the viewer
+        status = script.run_viewer(file_voxel, file_point, file_viewer, False)
+        self.assertTrue(status, msg="viewer failure")
+
+        # run the solver
+        status = script.run_solver(file_voxel, file_problem, file_solution)
+        self.assertTrue(status, msg="solver failure")
+
+        # run the plotter
+        status = script.run_plotter(file_solution, file_point, file_plotter, False)
+        self.assertTrue(status, msg="plotter failure")
+
+    def test_png_inductor(self):
+        """
+        Test workflow for png_inductor.
+        """
+
+        self._test_workflow("data_png_inductor")
+
+    def test_stl_inductor(self):
+        """
+        Test workflow for stl_inductor.
+        """
+
+        self._test_workflow("data_stl_inductor")
+
+    def test_voxel_slab(self):
+        """
+        Test workflow for voxel_slab.
+        """
+
+        self._test_workflow("data_voxel_slab")
+
+    def test_voxel_transformer(self):
+        """
+        Test workflow for voxel_transformer.
+        """
+
+        self._test_workflow("data_voxel_transformer")
+
+
+if __name__ == "__main__":
+    unittest.main()
