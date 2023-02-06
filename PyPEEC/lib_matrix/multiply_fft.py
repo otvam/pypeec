ca9282d649aed9717f58557c6174e15727831478
Thomas Guillod <guillod@otvam.ch>
2023-02-06 11:30:57 -0500

    coupling tensor multiplication

--- PyPEEC/lib_matrix/multiply_fft.py
+++ PyPEEC/lib_matrix/multiply_fft.py
@@ -71,9 +71,9 @@ def _get_tensor_circulant(mat, sign):
     mat_circulant[nx+1:2*nx, ny+1:2*ny, nz+1:2*nz, :] = mat[nx-1:0:-1, ny-1:0:-1, nz-1:0:-1, :]*sign[1:2, 1:2, 1:2, :]
 
     # get the FFT of the circulant tensor
-    mat_circulant_fft = fourier_transform.get_fft_tensor(mat_circulant, False)
+    mat_fft = fourier_transform.get_fft_tensor(mat_circulant, False)
 
-    return mat_circulant_fft
+    return mat_fft
 
 
 def get_prepare(mat, matrix_type):
@@ -95,12 +95,12 @@ def get_prepare(mat, matrix_type):
     else:
         raise ValueError("invalid matrix type")
 
-    mat_circulant_fft = _get_tensor_circulant(mat, sign)
+    mat_fft = _get_tensor_circulant(mat, sign)
 
-    return mat_circulant_fft
+    return mat_fft
 
 
-def get_multiply(idx_sel, vec_sel, mat_circulant_fft, matrix_type):
+def get_multiply(idx_sel, vec_sel, mat_fft, matrix_type):
     """
     Matrix-vector multiplication with FFT.
     The matrix is shaped as a FFT circulant tensor.
@@ -120,7 +120,7 @@ def get_multiply(idx_sel, vec_sel, mat_circulant_fft, matrix_type):
     """
 
     # get the tensor size
-    (nx, ny, nz, nd) = mat_circulant_fft.shape
+    (nx, ny, nz, nd) = mat_fft.shape
     nx = int(nx/2)
     ny = int(ny/2)
     nz = int(nz/2)
@@ -131,8 +131,22 @@ def get_multiply(idx_sel, vec_sel, mat_circulant_fft, matrix_type):
     # compute the FFT of the vector (result is the same size as the FFT circulant tensor)
     vec_all_fft = fourier_transform.get_fft_tensor(vec_all, True)
 
+    # init the results
+    res_all_fft = np.zeros((2*nx, 2*ny, 2*nz, nd), dtype=np.complex128)
+
     # matrix vector multiplication in frequency domain with the FFT circulant tensor
-    res_all_fft = mat_circulant_fft*vec_all_fft
+    if matrix_type == "3D":
+        res_all_fft[:, :, :, 0] = mat_fft[:, :, :, 0]*vec_all_fft[:, :, :, 0]
+    elif matrix_type == "4D_diag":
+        res_all_fft[:, :, :, 0] = mat_fft[:, :, :, 0]*vec_all_fft[:, :, :, 0]
+        res_all_fft[:, :, :, 1] = mat_fft[:, :, :, 1]*vec_all_fft[:, :, :, 1]
+        res_all_fft[:, :, :, 2] = mat_fft[:, :, :, 2]*vec_all_fft[:, :, :, 2]
+    elif matrix_type == "4D_off":
+        res_all_fft[:, :, :, 0] = +mat_fft[:, :, :, 2]*vec_all_fft[:, :, :, 1]-mat_fft[:, :, :, 1]*vec_all_fft[:, :, :, 2]
+        res_all_fft[:, :, :, 1] = -mat_fft[:, :, :, 2]*vec_all_fft[:, :, :, 0]+mat_fft[:, :, :, 0]*vec_all_fft[:, :, :, 2]
+        res_all_fft[:, :, :, 2] = +mat_fft[:, :, :, 1]*vec_all_fft[:, :, :, 0]-mat_fft[:, :, :, 0]*vec_all_fft[:, :, :, 1]
+    else:
+        raise ValueError("invalid matrix type")
 
     # compute the iFFT
     res_all = fourier_transform.get_ifft_tensor(res_all_fft, False)
