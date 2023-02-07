Correct matrix sign

PyPEEC/lib_solver/dense_matrix.py
@@ -39,15 +39,16 @@ def _get_coupling(d, idx, method, dimension):
     idx_tmp = idx[:, perm]
 
     # shift for the integration of the last dimension
-    idx_add = np.array([[0, 0, 0.5]])
+    idx_add_1 = np.array([[+0.5, +0.5, +0.5]])
+    idx_add_2 = np.array([[+0.5, +0.5, -0.5]])
 
     # get the partially integrated coefficients (5D integration)
     if method == "ana":
-        G_1 = green_function.get_green_ana(d_tmp, idx_tmp+idx_add, "5D")
-        G_2 = green_function.get_green_ana(d_tmp, idx_tmp-idx_add, "5D")
+        G_1 = green_function.get_green_ana(d_tmp, idx_tmp+idx_add_1, "5D")
+        G_2 = green_function.get_green_ana(d_tmp, idx_tmp+idx_add_2, "5D")
     elif method == "num":
-        G_1 = green_function.get_green_num(d_tmp, idx_tmp+idx_add, "5D")
-        G_2 = green_function.get_green_num(d_tmp, idx_tmp-idx_add, "5D")
+        G_1 = green_function.get_green_num(d_tmp, idx_tmp+idx_add_1, "5D")
+        G_2 = green_function.get_green_num(d_tmp, idx_tmp+idx_add_2, "5D")
     else:
         raise ValueError("invalid method")
 

PyPEEC/solver.py
@@ -137,11 +137,11 @@ def _run_main(data_solver):
         # compute the FFT circulant tensor (in order to make matrix-vector multiplication with FFT)
         L_tsr_c = mu*matrix_multiply.get_prepare_diag(idx_fc, L_tsr_c)
         Z_tsr_c = s*L_tsr_c
-        P_tsr_m = (1/(mu))*matrix_multiply.get_prepare_single(idx_vm, P_tsr_m)
+        P_tsr_m = (1/(mu*s))*matrix_multiply.get_prepare_single(idx_vm, P_tsr_m)
         R_vec_m = R_vec_m/(mu*s)
 
         vec = np.ones(len(idx_vm))
-        iden = s*np.diag(vec)
+        iden = np.diag(vec)
 
         K_c = matrix_multiply.get_prepare_cross(idx_fc, idx_fm, K_mutual)
         K_m = matrix_multiply.get_prepare_cross(idx_fm, idx_fc, K_mutual)
@@ -164,11 +164,11 @@ def _run_main(data_solver):
 
         a1 = np.zeros((len(idx_fc), len(idx_vm)))
         a2 = np.zeros((len(idx_fc), n_src))
-        E1 = np.block([[+Z_c, K_c, -1.0*A_red_c.transpose(), a1, a2]])
+        E1 = np.block([[+Z_c, +K_c, -1.0*A_red_c.transpose(), a1, a2]])
 
         a1 = np.zeros((len(idx_fm), len(idx_vc)))
         a2 = np.zeros((len(idx_fm), n_src))
-        E2 = np.block([[-K_m, Z_m, a1, -1.0*A_red_m.transpose(), a2]])
+        E2 = np.block([[-K_m, +Z_m, a1, -1.0*A_red_m.transpose(), a2]])
 
         a1 = np.zeros((len(idx_vm), len(idx_fc)))
         a2 = np.zeros((len(idx_vm), len(idx_vc)))
@@ -200,7 +200,7 @@ def _run_main(data_solver):
         L = np.imag(v)/(2*np.pi*freq)
 
         # get the energy for the different faces
-        M_f = matrix_multiply.get_multiply_diag(idx_fc, sol_fc, L_tsr_c)
+        M_f = np.matmul(L_tsr_c, sol_fc)
         Mf_m = np.matmul(K_c, sol_fm)/s
 
 
@@ -213,6 +213,8 @@ def _run_main(data_solver):
         print(2*W_tot)
 
         print(L)
+
+        np.matmul(K_m, sol_fc)
 
         raise RunError("STOP")
 