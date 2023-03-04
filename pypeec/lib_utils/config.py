d6623d9da2efd1a2fb82df59d5b81270f49b9c7a
Thomas Guillod <guillod@otvam.ch>
2023-03-04 17:17:37 -0500

    Add config switch single precision (no used in the code)

--- pypeec/lib_utils/config.py
+++ pypeec/lib_utils/config.py
@@ -6,68 +6,103 @@ Defined with global variables.
 __author__ = "Thomas Guillod"
 __copyright__ = "(c) Thomas Guillod - Dartmouth College"
 
+import os
 import sys
 import importlib.util
 import importlib.resources
+import numpy as np
 from pypeec.lib_utils import fileio
 from pypeec.lib_utils import datachecker
 from pypeec.lib_utils.error import FileError, CheckError
 
 
-def _check_data():
+def _update_data(data):
+    """
+    Parse and complete the config.
+    """
+
+    # get the number of cores
+    if data["FFT_OPTIONS"]["FFTS_WORKER"] is None:
+        data["FFT_OPTIONS"]["FFTS_WORKER"] = os.cpu_count()
+    if data["FFT_OPTIONS"]["FFTW_THREAD"] is None:
+        data["FFT_OPTIONS"]["FFTW_THREAD"] = os.cpu_count()
+
+    # get the data types
+    if data["USE_DOUBLE"]:
+        data["NB_TYPES"] = {"INT": np.int_, "FLOAT": np.float_, "COMPLEX": np.complex_}
+    else:
+        data["NB_TYPES"] = {"INT": np.intc, "FLOAT": np.single, "COMPLEX": np.csingle}
+
+    return data
+
+
+def _check_data(data):
     """
     Check that the config is valid.
     """
 
+    # check type
+    key_list = [
+        "LOGGING_OPTIONS",
+        "FFT_OPTIONS",
+        "FFT_LIBRARY",
+        "MATRIX_FACTORIZATION",
+        "MATRIX_MULTIPLICATION",
+        "PAUSE_GUI",
+        "USE_DOUBLE",
+        "USE_GPU",
+    ]
+    datachecker.check_dict("data", data, key_list=key_list)
+
     # check logging options
-    datachecker.check_string("FORMAT", LOGGING_OPTIONS["FORMAT"])
-    datachecker.check_string("FORMAT", LOGGING_OPTIONS["FORMAT"])
-    datachecker.check_string("LEVEL", LOGGING_OPTIONS["LEVEL"])
-    datachecker.check_integer("INDENTATION", LOGGING_OPTIONS["INDENTATION"])
-    datachecker.check_boolean("EXCEPTION_TRACE", LOGGING_OPTIONS["EXCEPTION_TRACE"])
-    datachecker.check_boolean("USE_COLOR", LOGGING_OPTIONS["USE_COLOR"])
-    datachecker.check_string("CL_DEBUG", LOGGING_OPTIONS["CL_DEBUG"])
-    datachecker.check_string("CL_INFO", LOGGING_OPTIONS["CL_INFO"])
-    datachecker.check_string("CL_WARNING", LOGGING_OPTIONS["CL_WARNING"])
-    datachecker.check_string("CL_ERROR", LOGGING_OPTIONS["CL_ERROR"])
-    datachecker.check_string("CL_CRITICAL", LOGGING_OPTIONS["CL_CRITICAL"])
-    datachecker.check_string("CL_RESET", LOGGING_OPTIONS["CL_RESET"])
+    datachecker.check_string("FORMAT", data["LOGGING_OPTIONS"]["FORMAT"])
+    datachecker.check_string("LEVEL", data["LOGGING_OPTIONS"]["LEVEL"])
+    datachecker.check_integer("INDENTATION", data["LOGGING_OPTIONS"]["INDENTATION"])
+    datachecker.check_boolean("EXCEPTION_TRACE", data["LOGGING_OPTIONS"]["EXCEPTION_TRACE"])
+    datachecker.check_boolean("USE_COLOR", data["LOGGING_OPTIONS"]["USE_COLOR"])
+    datachecker.check_string("CL_DEBUG", data["LOGGING_OPTIONS"]["CL_DEBUG"])
+    datachecker.check_string("CL_INFO", data["LOGGING_OPTIONS"]["CL_INFO"])
+    datachecker.check_string("CL_WARNING", data["LOGGING_OPTIONS"]["CL_WARNING"])
+    datachecker.check_string("CL_ERROR", data["LOGGING_OPTIONS"]["CL_ERROR"])
+    datachecker.check_string("CL_CRITICAL", data["LOGGING_OPTIONS"]["CL_CRITICAL"])
+    datachecker.check_string("CL_RESET", data["LOGGING_OPTIONS"]["CL_RESET"])
 
     # check FFT options
-    if FFT_OPTIONS["FFTS_WORKER"] is not None:
-        datachecker.check_integer("FFTS_WORKER", FFT_OPTIONS["FFTS_WORKER"])
-    if FFT_OPTIONS["FFTW_THREAD"] is not None:
-        datachecker.check_integer("FFTW_THREAD", FFT_OPTIONS["FFTW_THREAD"])
-    datachecker.check_integer("FFTW_BYTE_ALIGN", FFT_OPTIONS["FFTW_BYTE_ALIGN"])
-    datachecker.check_float("FFTW_CACHE_TIMEOUT", FFT_OPTIONS["FFTW_CACHE_TIMEOUT"])
+    if data["FFT_OPTIONS"]["FFTS_WORKER"] is not None:
+        datachecker.check_integer("FFTS_WORKER", data["FFT_OPTIONS"]["FFTS_WORKER"])
+    if data["FFT_OPTIONS"]["FFTW_THREAD"] is not None:
+        datachecker.check_integer("FFTW_THREAD", data["FFT_OPTIONS"]["FFTW_THREAD"])
+    datachecker.check_integer("FFTW_BYTE_ALIGN", data["FFT_OPTIONS"]["FFTW_BYTE_ALIGN"])
+    datachecker.check_float("FFTW_CACHE_TIMEOUT", data["FFT_OPTIONS"]["FFTW_CACHE_TIMEOUT"])
 
     # check other switches
-    datachecker.check_boolean("USE_GPU", USE_GPU)
-    datachecker.check_float("PAUSE_GUI", PAUSE_GUI)
-    datachecker.check_choice("FFT_LIBRARY", FFT_LIBRARY, ["SciPy", "FFTW"])
-    datachecker.check_choice("MATRIX_FACTORIZATION", MATRIX_FACTORIZATION, ["SuperLU", "UMFPACK"])
-    datachecker.check_choice("MATRIX_MULTIPLICATION", MATRIX_MULTIPLICATION, ["FFT", "DIRECT"])
+    datachecker.check_boolean("USE_GPU", data["USE_GPU"])
+    datachecker.check_boolean("USE_DOUBLE", data["USE_DOUBLE"])
+    datachecker.check_float("PAUSE_GUI", data["PAUSE_GUI"])
+    datachecker.check_choice("FFT_LIBRARY", data["FFT_LIBRARY"], ["SciPy", "FFTW"])
+    datachecker.check_choice("MATRIX_FACTORIZATION", data["MATRIX_FACTORIZATION"], ["SuperLU", "UMFPACK"])
+    datachecker.check_choice("MATRIX_MULTIPLICATION", data["MATRIX_MULTIPLICATION"], ["FFT", "DIRECT"])
 
 
-def _check_lib():
+def _check_lib(data):
     """
     Check that the required libraries are installed.
     """
 
     # check GPU
-    if USE_GPU:
+    if data["USE_GPU"]:
         lib = importlib.util.find_spec("cupy")
         if lib is None:
             raise FileError("Library CuPy is not installed and is activated in the config")
 
     # check FFTW
-    if FFT_LIBRARY == "FFTW":
+    if data["FFT_LIBRARY"] == "FFTW":
         lib = importlib.util.find_spec("pyfftw")
         if lib is None:
             raise FileError("Library FFTW is not installed and is activated in the config")
 
     # check library
-    if FFT_LIBRARY == "UMFPACK":
+    if data["FFT_LIBRARY"] == "UMFPACK":
         lib = importlib.util.find_spec("scikits.umfpack")
         if lib is None:
             raise FileError("Library UMFPACK is not installed and is activated in the config")
@@ -78,32 +113,21 @@ def set_config(file_config):
     Load a config file and store the data in global variables.
     """
 
-    # name of the global variables
-    key_list = [
-        "LOGGING_OPTIONS",
-        "FFT_OPTIONS",
-        "FFT_LIBRARY",
-        "MATRIX_FACTORIZATION",
-        "MATRIX_MULTIPLICATION",
-        "PAUSE_GUI",
-        "USE_GPU",
-    ]
-
     # parse the file
     data = fileio.load_config(file_config)
 
-    # check type
-    datachecker.check_dict("data", data, key_list=key_list)
-
-    # assign data to global variables
-    for key in key_list:
-        globals()[key] = data[key]
-
     # check the data integrity
-    _check_data()
+    _check_data(data)
 
     # check that the libraries are installed
-    _check_lib()
+    _check_lib(data)
+
+    # update the configuration
+    data = _update_data(data)
+
+    # assign data to global variables
+    for key in data:
+        globals()[key] = data[key]
 
 
 # init data to global variables
@@ -113,6 +137,8 @@ FFT_LIBRARY = None
 MATRIX_FACTORIZATION = None
 MATRIX_MULTIPLICATION = None
 PAUSE_GUI = None
+USE_DOUBLE = None
+NP_TYPES = None
 USE_GPU = None
 
 # load the default config files
