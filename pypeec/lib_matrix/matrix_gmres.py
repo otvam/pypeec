e36d45b5509a2f257dec5998bd9cdd9427a66662
Thomas Guillod <guillod@otvam.ch>
2023-03-14 21:41:07 -0400

    Simplify the GMRES (remove useless class)

--- pypeec/lib_matrix/matrix_gmres.py
+++ pypeec/lib_matrix/matrix_gmres.py
@@ -12,40 +12,6 @@ from pypeec.lib_utils import timelogger
 logger = timelogger.get_logger("GMRES")
 
 
-class _IterCounter:
-    """
-    Simple class used as a callback to count the number of iteration of the matrix solver.
-    """
-
-    def __init__(self):
-        """
-        Constructor.
-        Init the number of iteration.
-        """
-
-        self.n_iter = 0
-        self.res_iter = []
-
-    def get_callback(self, res):
-        """
-        Callback increasing the iteration count.
-        """
-
-        # update and save the iteration data
-        self.n_iter += 1
-        self.res_iter.append(res)
-
-        # log the results
-        logger.debug("matrix iter: i_iter = %d / res = %.3e" % (self.n_iter, res))
-
-    def get_n_iter(self):
-        """
-        Get the number of iterations.
-        """
-
-        return self.n_iter, self.res_iter
-
-
 def get_matrix_gmres(sys_op, pcd_op, rhs, gmres_options):
     """
     Solve a sparse equation system with GMRES.
@@ -58,12 +24,13 @@ def get_matrix_gmres(sys_op, pcd_op, rhs, gmres_options):
     n_between_restart = gmres_options["n_between_restart"]
     n_maximum_restart = gmres_options["n_maximum_restart"]
 
-    # object for counting the solver iterations (callback)
-    obj = _IterCounter()
+    # list for storing the residuum (callback)
+    conv = []
 
     # define callback
     def fct(res):
-        obj.get_callback(res)
+        conv.append(res)
+        logger.debug("matrix iter: iter = %d / res = %.3e" % (len(conv), res))
 
     # call the solver
     logger.debug("start matrix solver")
@@ -75,10 +42,7 @@ def get_matrix_gmres(sys_op, pcd_op, rhs, gmres_options):
     )
     logger.debug("exit matrix solver")
 
-    # get the number of iterations
-    (n_iter, res_iter) = obj.get_n_iter()
-
     # check for convergence
     status = flag == 0
 
-    return status, n_iter, res_iter, sol
+    return status, conv, sol
