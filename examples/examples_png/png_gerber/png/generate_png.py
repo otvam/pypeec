8d65929be2c30561c83751b1fcef45666f8399fb
Thomas Guillod <guillod@otvam.ch>
2023-04-03 18:38:51 -0400

    Better Python script for GERBER file handling

--- examples/examples_png/png_gerber/png/generate_png.py
+++ examples/examples_png/png_gerber/png/generate_png.py
@@ -17,11 +17,25 @@ import os
 import subprocess
 
 
-def get_gerbv_file(filename_gerbv, file_edge, stack):
+def get_gerbv_file(filename_gerbv, data_base, stack):
     """
     Create a "gerbv - Gerber Viewer" project file.
     """
 
+    # extract data
+    gerber_edge = data_base["gerber_edge"]
+    alpha_channel = data_base["alpha_channel"]
+    color_edge = data_base["color_edge"]
+    color_none = data_base["color_none"]
+    color_def = data_base["color_def"]
+    gerber_def = data_base["gerber_def"]
+
+    # get name
+    color_edge = color_def[color_edge]
+    color_none = color_def[color_none]
+    gerber_edge = gerber_def[gerber_edge]
+
+    # create the file
     with open(filename_gerbv, "w") as fid:
         # add header
         fid.write('(gerbv-file-version! "2.0A")\n')
@@ -30,43 +44,48 @@ def get_gerbv_file(filename_gerbv, file_edge, stack):
         for i, stack_tmp in enumerate(stack):
             # extract data
             layer = i+1
-            file = stack_tmp["file"]
-            (cr, cg, cb) = stack_tmp["color"]
+            gerber = stack_tmp["gerber"]
+            color = stack_tmp["color"]
+
+            # get color
+            color = color_def[color]
+            gerber = gerber_def[gerber]
 
             # add layer
-            fid.write('(define-layer! %d (cons \'filename "%s")\n' % (layer, file))
+            fid.write('(define-layer! %d (cons \'filename "%s")\n' % (layer, gerber))
             fid.write('\t(cons \'visible #t)\n')
-            fid.write('\t(cons \'color #(%d %d %d))\n' % (cr, cg, cb))
-            fid.write('\t(cons \'alpha #(65535))\n')
+            fid.write('\t(cons \'color #(%d %d %d))\n' % tuple(color))
+            fid.write('\t(cons \'alpha #(%d))\n' % alpha_channel)
             fid.write(')\n')
 
         # add board edges
-        fid.write('(define-layer! 0 (cons \'filename "%s")\n' % file_edge)
+        fid.write('(define-layer! 0 (cons \'filename "%s")\n' % gerber_edge)
         fid.write('\t(cons \'visible #t)\n')
-        fid.write('\t(cons \'color #(0 0 0))\n')
-        fid.write('\t(cons \'alpha #(65535))\n')
+        fid.write('\t(cons \'color #(%d %d %d))\n' % tuple(color_edge))
+        fid.write('\t(cons \'alpha #(%d))\n' % alpha_channel)
         fid.write(')\n')
 
         # add background
         fid.write('(define-layer! -1 (cons \'filename "%s")\n' % filename_gerbv)
-        fid.write('\t(cons \'color #(65535 65535 65535))\n')
+        fid.write('\t(cons \'color #(%d %d %d))\n' % tuple(color_none))
+        fid.write('\t(cons \'alpha #(%d))\n' % alpha_channel)
         fid.write(')\n')
 
         # add rendering settings
         fid.write('(set-render-type! 3)\n')
 
 
-def get_png(name, border, resolution, file_edge, stack):
+def get_png(layer, name, border, resolution, data_base, stack):
     """
-    Transform GERBER files into a PNG file.
+    Transform GERBER files into a PNG file for a given layer.
     """
 
     # create the file names
-    filename_gerbv = name + ".gvp"
-    filename_png = name + ".png"
+    filename_gerbv = name + "_" + layer + ".gvp"
+    filename_png = name + "_" + layer + ".png"
 
     # get the project files with the GERBER files
-    get_gerbv_file(filename_gerbv, file_edge, stack)
+    get_gerbv_file(filename_gerbv, data_base, stack)
 
     # transform the GERBER files into a PNG file
     cmd = "gerbv -p %s -o %s -x png --border %d --dpi %d 2>/dev/null" % (
@@ -85,40 +104,72 @@ def get_png(name, border, resolution, file_edge, stack):
     os.remove(filename_gerbv)
 
 
+def get_layer(name, border, resolution, data_base):
+    """
+    Transform GERBER files into PNG files.
+    """
+
+    # front
+    stack = [
+        {"gerber": "drill", "color": "none"},
+        {"gerber": "front", "color": "copper"},
+    ]
+    get_png("front", name, border, resolution, data_base, stack)
+
+    # back
+    stack = [
+        {"gerber": "drill", "color": "none"},
+        {"gerber": "back", "color": "copper"},
+    ]
+    get_png("back", name, border, resolution, data_base, stack)
+
+    # terminal
+    stack = [
+        {"gerber": "drill", "color": "none"},
+        {"gerber": "sink", "color": "sink"},
+        {"gerber": "src", "color": "src"},
+    ]
+    get_png("terminal", name, border, resolution, data_base, stack)
+
+    # via
+    stack = [
+        {"gerber": "drill", "color": "none"},
+        {"gerber": "via", "color": "copper"},
+    ]
+    get_png("via", name, border, resolution, data_base, stack)
+
+
 if __name__ == "__main__":
+    # ######################## get the colors / GERBER files
+    color_def = {
+        "none":  (65535, 65535, 65535),
+        "edge":  (0, 0, 0),
+        "copper": (65535, 0, 0),
+        "sink":  (0, 0, 65535),
+        "src": (0, 65535, 0),
+    }
+    gerber_def = {
+        "edge":  "gerber/generate_gerber-edge.gbr",
+        "front": "gerber/generate_gerber-front_copper.gbr",
+        "back": "gerber/generate_gerber-back_copper.gbr",
+        "src": "gerber/generate_gerber-src.gbr",
+        "sink": "gerber/generate_gerber-sink.gbr",
+        "drill":  "gerber/generate_gerber-PTH.drl",
+        "via":  "gerber/generate_gerber-VIA.drl",
+    }
+
     # ######################## get the variables
+    name = "gerbv"
     border = 10
     resolution = 1500
-    file_edge = "gerber/generate_gerber-edge.gbr"
+    data_base = {
+        "gerber_edge": "edge",
+        "alpha_channel": 65535,
+        "color_edge": "edge",
+        "color_none": "none",
+        "color_def": color_def,
+        "gerber_def": gerber_def
+    }
 
-    # ######################## gerbv_front
-    name = "gerbv_front"
-    stack = [
-        {"file": "gerber/generate_gerber-PTH.drl", "color": (65535, 65535, 65535)},
-        {"file": "gerber/generate_gerber-front_copper.gbr", "color": (65535, 0, 0)},
-    ]
-    get_png(name, border, resolution, file_edge, stack)
-
-    # ######################## gerbv_back
-    name = "gerbv_back"
-    stack = [
-        {"file": "gerber/generate_gerber-PTH.drl", "color": (65535, 65535, 65535)},
-        {"file": "gerber/generate_gerber-back_copper.gbr", "color": (65535, 0, 0)},
-    ]
-    get_png(name, border, resolution, file_edge, stack)
-
-    # ######################## gerbv_terminal
-    name = "gerbv_terminal"
-    stack = [
-        {"file": "gerber/generate_gerber-sink.gbr", "color": (0, 0, 65535)},
-        {"file": "gerber/generate_gerber-src.gbr", "color": (0, 65535, 0)},
-    ]
-    get_png(name, border, resolution, file_edge, stack)
-
-    # ######################## gerbv_via
-    name = "gerbv_via"
-    stack = [
-        {"file": "gerber/generate_gerber-PTH.drl", "color": (65535, 65535, 65535)},
-        {"file": "gerber/generate_gerber-VIA.drl", "color": (65535, 0, 0)},
-    ]
-    get_png(name, border, resolution, file_edge, stack)
+    # ######################## run
+    get_layer(name, border, resolution, data_base)
