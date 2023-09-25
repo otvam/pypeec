113568c528f130df4fbd54799e19477b7e617e28
Thomas Guillod <guillod@otvam.ch>
2023-09-25 18:51:52 -0400

    bug for checking the voxel structure bounds (shape mesher)

--- pypeec/lib_check/check_data_geometry_shape.py
+++ pypeec/lib_check/check_data_geometry_shape.py
@@ -126,8 +126,8 @@ def _check_param(param):
     datachecker.check_float("dz", param["dz"], is_positive=True, can_be_zero=False)
     datachecker.check_float("tol", param["tol"], is_positive=True, can_be_zero=False)
     datachecker.check_float("cz", param["cz"], is_positive=False, can_be_zero=True)
-    datachecker.check_float_array("xy_min", param["xy_min"], size=3, is_positive=False, can_be_zero=True, can_be_none=True)
-    datachecker.check_float_array("xy_max", param["xy_max"], size=3, is_positive=False, can_be_zero=True, can_be_none=True)
+    datachecker.check_float_array("xy_min", param["xy_min"], size=2, is_positive=False, can_be_zero=True, can_be_none=True)
+    datachecker.check_float_array("xy_max", param["xy_max"], size=2, is_positive=False, can_be_zero=True, can_be_none=True)
 
 
 def check_data_voxelize(data_voxelize):
