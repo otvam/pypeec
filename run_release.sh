280caf0dee577b6a6d8e9fd3152772f60f7d3929
Thomas Guillod <guillod@otvam.ch>
2024-01-04 09:19:28 -0500

    check test before release

--- run_release.sh
+++ run_release.sh
@@ -14,20 +14,20 @@ set -o pipefail
 
 function create_tag {
   echo "======================================================================"
-  echo "Create tag"
+  echo "CREATE TAG"
   echo "======================================================================"
 
   # create a tag
   git tag -a $VER -m "$MSG"
-
-  # push the tags
-  git push origin --tags
 }
 
 function create_release {
   echo "======================================================================"
-  echo "Create release"
+  echo "CREATE RELEASE"
   echo "======================================================================"
+
+  # push the tags
+  git push origin --tags
 
   # create a release
   gh release create $VER --title $VER --notes "$MSG"
@@ -35,7 +35,7 @@ function create_release {
 
 function upload_documentation {
   echo "======================================================================"
-  echo "Upload documentation"
+  echo "UPLOAD DOCUMENTATION"
   echo "======================================================================"
 
   # init clean data
@@ -61,15 +61,42 @@ function upload_documentation {
 
 function upload_package {
   echo "======================================================================"
-  echo "Upload package"
+  echo "UPLOAD PACKAGE"
   echo "======================================================================"
 
   # upload to PyPi
   twine upload dist/*
 }
 
+function run_build_test {
+  # init status
+  ret=0
+
+  # check build
+  ./run_build.sh
+  ret=$(( ret || $? ))
+
+  # check tests
+  ./run_tests.sh
+  ret=$(( ret || $? ))
+
+  if (( $ret != 0 ))
+  then
+    echo "======================================================================"
+    echo "RELEASE FAILURE"
+    echo "======================================================================"
+
+    # clean tags
+    git tag -d $VER
+
+    # force quit
+    exit $ret
+  fi
+}
+
 # get the version and commit message
-if [ "$#" -eq 2 ]; then
+if [ "$#" -eq 2 ]
+then
   VER=$1
   MSG=$2
 else
@@ -79,8 +106,8 @@ fi
 
 # run the code
 create_tag
+run_build_test
 create_release
-./run_build.sh
 upload_documentation
 upload_package
 
