04abc959247e1091ea570a99267c97cf716f4849
Thomas Guillod <guillod@otvam.ch>
2024-08-11 23:23:21 -0400

    new tests

--- tests/test_stl.py
+++ tests/test_stl.py
@@ -18,6 +18,7 @@ class TestStl(test_workflow.TestWorkflow):
 name_list = [
     "examples_stl/inductor_air",
     "examples_stl/inductor_core",
+    "examples_stl/inductor_toroid",
     "examples_stl/transformer",
 ]
 
