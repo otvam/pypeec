04d325f7342fb54b511469bab37a7c4d67a10b53
Thomas Guillod <guillod@otvam.ch>
2024-12-28 18:46:25 -0500

    cadquery file for transformer and toroid examples

---
+++ examples/examples_stl/inductor_toroid/stl/generate_stl.py
@@ -0,0 +1,59 @@
+"""
+Build the CAD objects and export STL files.
+"""
+
+__author__ = "Thomas Guillod"
+__copyright__ = "Thomas Guillod - Dartmouth College"
+__license__ = "Mozilla Public License Version 2.0"
+
+import sys
+import cadquery as cq
+
+
+def get_cad():
+    """
+    Get the different CAD objects.
+    """
+
+    core = cq.Workplane("XY").cylinder(height=8.0, radius=16.0)
+    core = core.circle(10.0).cutThruAll()
+
+    coil = cq.Workplane("XZ").moveTo(13, 0.0).box(20.0, 20.0, 8.0)
+    coil = coil.moveTo(13, 0).rect(12.0, 12.0).cutThruAll()
+    coil = coil.moveTo(21, 0).rect(4.0, 4.0).cutThruAll()
+
+    src = cq.Workplane("XZ").moveTo(21.0, +1.5).box(4.0, 1.0, 8.0)
+    sink = cq.Workplane("XZ").moveTo(21.0, -1.5).box(4.0, 1.0, 8.0)
+
+    cad_dict = {
+        "core": core,
+        "coil": coil,
+        "src": src,
+        "sink": sink,
+    }
+
+    return cad_dict
+
+
+def write_cad(cad_dict, tol):
+    """
+    Export CAD objects to STL files.
+    """
+
+    for tag, cad in cad_dict.items():
+        if cad is not None:
+            cad.val().exportStl("%s.stl" % tag, tolerance=tol)
+
+
+if __name__ == "__main__":
+    # get the object tolerance
+    tol = 1e-3
+
+    # get the CAD objects
+    cad_dict = get_cad()
+
+    # save STL/STEP files
+    write_cad(cad_dict, tol)
+
+    # exit
+    sys.exit(0)
---
+++ examples/examples_stl/transformer/stl/generate_stl.py
@@ -0,0 +1,60 @@
+"""
+Build the CAD objects and export STL files.
+"""
+
+__author__ = "Thomas Guillod"
+__copyright__ = "Thomas Guillod - Dartmouth College"
+__license__ = "Mozilla Public License Version 2.0"
+
+import sys
+import cadquery as cq
+
+
+def get_cad():
+    """
+    Get the different CAD objects.
+    """
+
+    coil = cq.Workplane("XY").box(80.0, 40, 1.0)
+    coil = coil.rect(72.0, 32.0).cutThruAll()
+    coil = coil.moveTo(48.0, +4.0).box(16.0, 4.0, 1.0)
+    coil = coil.moveTo(48.0, -4.0).box(16.0, 4.0, 1.0)
+    coil = coil.moveTo(38.0, 0.0).rect(4.0, 4.0).cutThruAll()
+
+    src = cq.Workplane("XY").moveTo(56.5, +4.0).box(1.0, 4.0, 1.0)
+    sink = cq.Workplane("XY").moveTo(56.5, -4.0).box(1.0, 4.0, 1.0)
+
+    cad_dict = {
+        "pri_coil": coil.translate((0.0, 0.0, +2.0)),
+        "pri_src": src.translate((0.0, 0.0, +2.0)),
+        "pri_sink": sink.translate((0.0, 0.0, +2.0)),
+        "sec_coil": coil.translate((0.0, 0.0, -2.0)),
+        "sec_src": src.translate((0.0, 0.0, -2.0)),
+        "sec_sink": sink.translate((0.0, 0.0, -2.0)),
+    }
+
+    return cad_dict
+
+
+def write_cad(cad_dict, tol):
+    """
+    Export CAD objects to STL files.
+    """
+
+    for tag, cad in cad_dict.items():
+        if cad is not None:
+            cad.val().exportStl("%s.stl" % tag, tolerance=tol)
+
+
+if __name__ == "__main__":
+    # get the object tolerance
+    tol = 1e-3
+
+    # get the CAD objects
+    cad_dict = get_cad()
+
+    # save STL/STEP files
+    write_cad(cad_dict, tol)
+
+    # exit
+    sys.exit(0)
