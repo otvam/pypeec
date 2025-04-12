c62a5a9956685ee3dfc4abb49ae656941471d902
Thomas Guillod <guillod@otvam.ch>
2025-04-12 16:28:24 -0400

    run tests with png (should fail)

--- tests/code/test_pypeec.py
+++ tests/code/test_pypeec.py
@@ -109,7 +109,7 @@ def run_workflow(name, use_script):
                 "--viewer",
                 file_viewer,
                 "--plot_mode",
-                "qt",
+                "png",
             ]
             argv_so = [
                 "--quiet",
@@ -131,7 +131,7 @@ def run_workflow(name, use_script):
                 "--plotter",
                 file_plotter,
                 "--plot_mode",
-                "qt",
+                "png",
             ]
 
             # run the scripts
@@ -153,12 +153,12 @@ def run_workflow(name, use_script):
             pypeec.run_viewer_file(
                 file_voxel=file_voxel,
                 file_viewer=file_viewer,
-                plot_mode="qt",
+                plot_mode="png",
             )
             pypeec.run_plotter_file(
                 file_solution=file_solution,
                 file_plotter=file_plotter,
-                plot_mode="qt",
+                plot_mode="png",
             )
 
         # load the files
