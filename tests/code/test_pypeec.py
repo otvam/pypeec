ea1ef2114c9e4b0cab4fabfe7e0c834c09bddf61
Thomas Guillod <guillod@otvam.ch>
2025-04-23 11:30:00 -0400

    refactor tests

--- tests/code/test_pypeec.py
+++ tests/code/test_pypeec.py
@@ -13,6 +13,11 @@ import scilogger
 import scisave
 import pypeec
 
+# crash on warnings and ignore deprecation
+warnings.filterwarnings("error")
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+warnings.filterwarnings("ignore", category=PendingDeprecationWarning)
+
 # disable logging
 scilogger.disable()
 
@@ -20,43 +25,124 @@ scilogger.disable()
 PATH_ROOT = os.path.dirname(__file__)
 
 
-def _create_temp_file():
+def _get_run_mesher(use_script, file_geometry, file_voxel):
     """
-    Get a temporary file.
-    """
-
-    (_, filename) = tempfile.mkstemp(suffix=".mpk")
-
-    return filename
-
-
-def _delete_temp_file(filename):
-    """
-    Delete a temporary file.
+    Run the mesher.
     """
 
-    try:
-        os.remove(filename)
-    except OSError:
-        pass
+    if use_script:
+        status = pypeec.run_arguments(
+            [
+                "--quiet",
+                "mesher",
+                "--geometry",
+                file_geometry,
+                "--voxel",
+                file_voxel,
+            ]
+        )
+        if status != 0:
+            raise RuntimeError("invalid return code for the mesher")
+    else:
+        pypeec.run_mesher_file(
+            file_geometry=file_geometry,
+            file_voxel=file_voxel,
+        )
 
 
-def _run_script(argv):
+def _get_run_solver(use_script, file_problem, file_tolerance, file_voxel, file_solution):
     """
-    Run the command line script with arguments.
+    Run the solver.
     """
 
-    status = pypeec.run_arguments(argv)
-    if status != 0:
-        raise RuntimeError("invalid return code for the script")
+    if use_script:
+        status = pypeec.run_arguments(
+            [
+                "--quiet",
+                "solver",
+                "--voxel",
+                file_voxel,
+                "--problem",
+                file_problem,
+                "--tolerance",
+                file_tolerance,
+                "--solution",
+                file_solution,
+            ]
+        )
+        if status != 0:
+            raise RuntimeError("invalid return code for the solver")
+    else:
+        pypeec.run_solver_file(
+            file_voxel=file_voxel,
+            file_problem=file_problem,
+            file_tolerance=file_tolerance,
+            file_solution=file_solution,
+        )
+
+
+def _get_run_viewer(use_script, file_viewer, file_voxel):
+    """
+    Run the viewer.
+    """
+
+    if use_script:
+        status = pypeec.run_arguments(
+            [
+                "--quiet",
+                "viewer",
+                "--voxel",
+                file_voxel,
+                "--viewer",
+                file_viewer,
+                "--plot_mode",
+                "debug",
+            ]
+        )
+        if status != 0:
+            raise RuntimeError("invalid return code for the viewer")
+    else:
+        pypeec.run_viewer_file(
+            file_voxel=file_voxel,
+            file_viewer=file_viewer,
+            plot_mode="debug",
+        )
+
+
+def _get_run_plotter(use_script, file_plotter, file_solution):
+    """
+    Run the plotter.
+    """
+
+    if use_script:
+        status = pypeec.run_arguments(
+            [
+                "--quiet",
+                "plotter",
+                "--solution",
+                file_solution,
+                "--plotter",
+                file_plotter,
+                "--plot_mode",
+                "debug",
+            ]
+        )
+        if status != 0:
+            raise RuntimeError("invalid return code for the plotter")
+    else:
+        pypeec.run_plotter_file(
+            file_solution=file_solution,
+            file_plotter=file_plotter,
+            plot_mode="debug",
+        )
 
 
 def run_workflow(name, use_script):
     """
     Run the complete workflow:
         - Run the mesher.
-        - Run the viewer.
         - Run the solver.
+        - Run the viewer.
         - Run the plotter.
 
     The intermediate file are stored with temporary files.
@@ -66,11 +152,6 @@ def run_workflow(name, use_script):
         - With the command line script (pypeec.script).
         - With the API (pypeec.main).
     """
-
-    # crash on warnings and ignore deprecation
-    warnings.filterwarnings("error")
-    warnings.filterwarnings("ignore", category=DeprecationWarning)
-    warnings.filterwarnings("ignore", category=PendingDeprecationWarning)
 
     # construct the folder path for the examples
     folder_examples = os.path.join(PATH_ROOT, "..", "..", "examples")
@@ -85,88 +166,23 @@ def run_workflow(name, use_script):
     file_tolerance = os.path.join(folder_examples, "config", "tolerance.yaml")
 
     # get the temporary files
-    file_voxel = _create_temp_file()
-    file_solution = _create_temp_file()
+    (_, file_voxel) = tempfile.mkstemp(suffix=".mpk")
+    (_, file_solution) = tempfile.mkstemp(suffix=".mpk")
 
-    # run the code
+    # run the workflow and load the results
     try:
         # run the workflow
-        if use_script:
-            # get the arguments
-            argv_me = [
-                "--quiet",
-                "mesher",
-                "--geometry",
-                file_geometry,
-                "--voxel",
-                file_voxel,
-            ]
-            argv_vi = [
-                "--quiet",
-                "viewer",
-                "--voxel",
-                file_voxel,
-                "--viewer",
-                file_viewer,
-                "--plot_mode",
-                "debug",
-            ]
-            argv_so = [
-                "--quiet",
-                "solver",
-                "--voxel",
-                file_voxel,
-                "--problem",
-                file_problem,
-                "--tolerance",
-                file_tolerance,
-                "--solution",
-                file_solution,
-            ]
-            argv_pl = [
-                "--quiet",
-                "plotter",
-                "--solution",
-                file_solution,
-                "--plotter",
-                file_plotter,
-                "--plot_mode",
-                "debug",
-            ]
-
-            # run the scripts
-            _run_script(argv_me)
-            _run_script(argv_vi)
-            _run_script(argv_so)
-            _run_script(argv_pl)
-        else:
-            pypeec.run_mesher_file(
-                file_geometry=file_geometry,
-                file_voxel=file_voxel,
-            )
-            pypeec.run_solver_file(
-                file_voxel=file_voxel,
-                file_problem=file_problem,
-                file_tolerance=file_tolerance,
-                file_solution=file_solution,
-            )
-            pypeec.run_viewer_file(
-                file_voxel=file_voxel,
-                file_viewer=file_viewer,
-                plot_mode="debug",
-            )
-            pypeec.run_plotter_file(
-                file_solution=file_solution,
-                file_plotter=file_plotter,
-                plot_mode="debug",
-            )
+        _get_run_mesher(use_script, file_geometry, file_voxel)
+        _get_run_solver(use_script, file_problem, file_tolerance, file_voxel, file_solution)
+        _get_run_plotter(use_script, file_plotter, file_solution)
+        _get_run_viewer(use_script, file_viewer, file_voxel)
 
         # load the files
         data_voxel = scisave.load_data(file_voxel)
         data_solution = scisave.load_data(file_solution)
     finally:
         # close the temporary files
-        _delete_temp_file(file_voxel)
-        _delete_temp_file(file_solution)
+        os.remove(file_voxel)
+        os.remove(file_solution)
 
     return data_voxel, data_solution
